c2139b68a2225c5a6211229ef88a1ba956fe63ee
Scott Lau <exceedego@126.com>
2021-05-12 11:33:28 +0800

    更新依赖版本

--- setup.py
+++ setup.py
@@ -76,8 +76,8 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'sc-utilities>=0.0.2',
-        'sc-config>=0.0.5',
+        'sc-utilities>=0.0.3',
+        'sc-config>=0.0.6',
         'pandas>=1.2.4',
         'openpyxl>=3.0.7',
     ],
